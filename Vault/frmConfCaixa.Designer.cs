acebaa5ae451d3a2c90d030bd23f19819ab355fd
Sam Notebook <dragoonbr@gmail.com>
2017-01-15 19:59:18 -0200

    um monte de mudanças (ficou chapado!)

--- Vault/frmConfCaixa.Designer.cs
+++ Vault/frmConfCaixa.Designer.cs
@@ -28,64 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gbx0 = new System.Windows.Forms.GroupBox();
-            this.tbSalAt0 = new System.Windows.Forms.TextBox();
+            this.gbxCaixa = new System.Windows.Forms.GroupBox();
+            this.tbSaldoAtual = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.cbxOp0 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.cbxOperador = new System.Windows.Forms.ComboBox();
+            this.lblOperador = new System.Windows.Forms.Label();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.gbx0.SuspendLayout();
+            this.tbDinheiro = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbxCheckout = new System.Windows.Forms.ComboBox();
+            this.lblCheckout = new System.Windows.Forms.Label();
+            this.gbxCaixa.SuspendLayout();
             this.SuspendLayout();
             // 
-            // gbx0
+            // gbxCaixa
             // 
-            this.gbx0.Controls.Add(this.tbSalAt0);
-            this.gbx0.Controls.Add(this.label2);
-            this.gbx0.Controls.Add(this.cbxOp0);
-            this.gbx0.Controls.Add(this.label1);
-            this.gbx0.Location = new System.Drawing.Point(12, 12);
-            this.gbx0.Name = "gbx0";
-            this.gbx0.Size = new System.Drawing.Size(201, 354);
-            this.gbx0.TabIndex = 0;
-            this.gbx0.TabStop = false;
-            this.gbx0.Text = "Caixa";
+            this.gbxCaixa.Controls.Add(this.cbxCheckout);
+            this.gbxCaixa.Controls.Add(this.lblCheckout);
+            this.gbxCaixa.Controls.Add(this.tbDinheiro);
+            this.gbxCaixa.Controls.Add(this.label3);
+            this.gbxCaixa.Controls.Add(this.tbSaldoAtual);
+            this.gbxCaixa.Controls.Add(this.label2);
+            this.gbxCaixa.Controls.Add(this.cbxOperador);
+            this.gbxCaixa.Controls.Add(this.lblOperador);
+            this.gbxCaixa.Location = new System.Drawing.Point(12, 12);
+            this.gbxCaixa.Name = "gbxCaixa";
+            this.gbxCaixa.Size = new System.Drawing.Size(201, 354);
+            this.gbxCaixa.TabIndex = 0;
+            this.gbxCaixa.TabStop = false;
+            this.gbxCaixa.Text = "Caixa";
+            this.gbxCaixa.Enter += new System.EventHandler(this.gbxCaixa_Enter);
             // 
-            // tbSalAt0
+            // tbSaldoAtual
             // 
-            this.tbSalAt0.Location = new System.Drawing.Point(73, 49);
-            this.tbSalAt0.MaxLength = 10;
-            this.tbSalAt0.Name = "tbSalAt0";
-            this.tbSalAt0.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.tbSalAt0.Size = new System.Drawing.Size(122, 20);
-            this.tbSalAt0.TabIndex = 6;
-            this.tbSalAt0.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbSalAt0_KeyPress);
-            this.tbSalAt0.Leave += new System.EventHandler(this.tbSalAt0_Leave);
+            this.tbSaldoAtual.Location = new System.Drawing.Point(73, 79);
+            this.tbSaldoAtual.MaxLength = 10;
+            this.tbSaldoAtual.Name = "tbSaldoAtual";
+            this.tbSaldoAtual.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tbSaldoAtual.Size = new System.Drawing.Size(122, 20);
+            this.tbSaldoAtual.TabIndex = 6;
+            this.tbSaldoAtual.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbSalAt0_KeyPress);
+            this.tbSaldoAtual.Leave += new System.EventHandler(this.tbSalAt0_Leave);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 52);
+            this.label2.Location = new System.Drawing.Point(6, 82);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Saldo Atual";
             // 
-            // cbxOp0
+            // cbxOperador
             // 
-            this.cbxOp0.FormattingEnabled = true;
-            this.cbxOp0.Location = new System.Drawing.Point(73, 21);
-            this.cbxOp0.Name = "cbxOp0";
-            this.cbxOp0.Size = new System.Drawing.Size(122, 21);
-            this.cbxOp0.TabIndex = 3;
+            this.cbxOperador.FormattingEnabled = true;
+            this.cbxOperador.Location = new System.Drawing.Point(73, 51);
+            this.cbxOperador.Name = "cbxOperador";
+            this.cbxOperador.Size = new System.Drawing.Size(122, 21);
+            this.cbxOperador.TabIndex = 3;
+            this.cbxOperador.SelectedIndexChanged += new System.EventHandler(this.cbxOperador_SelectedIndexChanged);
             // 
-            // label1
+            // lblOperador
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Operador";
+            this.lblOperador.AutoSize = true;
+            this.lblOperador.Location = new System.Drawing.Point(6, 54);
+            this.lblOperador.Name = "lblOperador";
+            this.lblOperador.Size = new System.Drawing.Size(51, 13);
+            this.lblOperador.TabIndex = 2;
+            this.lblOperador.Text = "Operador";
+            this.lblOperador.DoubleClick += new System.EventHandler(this.lblOperador_DoubleClick);
             // 
             // groupBox5
             // 
@@ -96,31 +107,74 @@
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Caixa";
             // 
+            // tbDinheiro
+            // 
+            this.tbDinheiro.Location = new System.Drawing.Point(73, 107);
+            this.tbDinheiro.MaxLength = 10;
+            this.tbDinheiro.Name = "tbDinheiro";
+            this.tbDinheiro.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tbDinheiro.Size = new System.Drawing.Size(122, 20);
+            this.tbDinheiro.TabIndex = 8;
+            this.tbDinheiro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbDinheiro_KeyPress);
+            this.tbDinheiro.Leave += new System.EventHandler(this.tbDinheiro_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Dinheiro";
+            // 
+            // cbxCheckout
+            // 
+            this.cbxCheckout.FormattingEnabled = true;
+            this.cbxCheckout.Location = new System.Drawing.Point(73, 23);
+            this.cbxCheckout.Name = "cbxCheckout";
+            this.cbxCheckout.Size = new System.Drawing.Size(122, 21);
+            this.cbxCheckout.TabIndex = 10;
+            this.cbxCheckout.SelectedIndexChanged += new System.EventHandler(this.cbxCheckout_SelectedIndexChanged);
+            // 
+            // lblCheckout
+            // 
+            this.lblCheckout.AutoSize = true;
+            this.lblCheckout.Location = new System.Drawing.Point(6, 26);
+            this.lblCheckout.Name = "lblCheckout";
+            this.lblCheckout.Size = new System.Drawing.Size(53, 13);
+            this.lblCheckout.TabIndex = 9;
+            this.lblCheckout.Text = "Checkout";
+            this.lblCheckout.DoubleClick += new System.EventHandler(this.lblCheckout_DoubleClick);
+            // 
             // frmConfCaixa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(225, 416);
             this.Controls.Add(this.groupBox5);
-            this.Controls.Add(this.gbx0);
+            this.Controls.Add(this.gbxCaixa);
             this.Name = "frmConfCaixa";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Conferência de Caixa";
             this.Load += new System.EventHandler(this.frmConfCaixa_Load);
-            this.gbx0.ResumeLayout(false);
-            this.gbx0.PerformLayout();
+            this.gbxCaixa.ResumeLayout(false);
+            this.gbxCaixa.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox gbx0;
-        private System.Windows.Forms.ComboBox cbxOp0;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbxCaixa;
+        private System.Windows.Forms.ComboBox cbxOperador;
+        private System.Windows.Forms.Label lblOperador;
         private System.Windows.Forms.GroupBox groupBox5;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox tbSalAt0;
+        private System.Windows.Forms.TextBox tbSaldoAtual;
+        private System.Windows.Forms.TextBox tbDinheiro;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbxCheckout;
+        private System.Windows.Forms.Label lblCheckout;
     }
 }
--- Vault/frmConfCaixa.cs
+++ Vault/frmConfCaixa.cs
@@ -10,19 +10,22 @@ namespace Vault
         public frmConfCaixa()
         {
             InitializeComponent();
-        }
+        }                                                                                           //Ao tornar uma unica instância de dos campos, , trocando somente o numero
+                                              //TODO:                                               //do checkout, precisamos criar uma forma de controlar as alterações.
+                                                                                                    //Levando em consideração o fato de ás vezes os operadores de caixa
+                                                                                                    //trocarem de checkout.
+                                                                                                    //provavelmente inserir uma comboBox, e alterar o titulo da gbox
+                                                                                                    //conforme o checkout e o operador.
 
-
-        public void carregaDados(ComboBox cbx, GroupBox gbx, int index)
+        public void carregaDados(ComboBox op,ComboBox chkOut)
         {
             DatFileController dc = new DatFileController(); //inicializa a classe que manipula os  dados.
-
-            gbx0.Text = dc.read(Config.checkout)[index].ToString(); //carrega o nome do caixa pelo INDEX 
-
             for (int i = 0; i < dc.read(Config.operador).Count; i++)//carrega operadores de caixa para o combobox
-                cbx.Items.Add(dc.read(Config.operador)[i]);
+                op.Items.Add(dc.read(Config.operador)[i]);
+            for (int i = 0; i < dc.read(Config.checkout).Count; i++)//carrega checkouts de caixa para o combobox
+                chkOut.Items.Add(dc.read(Config.checkout)[i]);
 
-            cbx.SelectedIndex = index; //seleciona o valor padrão do INDEX
+          
         }
 
 
@@ -30,15 +33,16 @@ namespace Vault
         private void frmConfCaixa_Load(object sender, EventArgs e)
         {
             DatFileController dc = new DatFileController();
+            carregaDados(cbxOperador, cbxCheckout);
 
-            //caixa 101 - default Nalva
-            carregaDados(cbxOp0, gbx0, 0);
+
+            
 
         }
 
         private void tbSalAt0_Leave(object sender, EventArgs e)
         {
-            tbFormat(tbSalAt0);
+            tbFormat(tbSaldoAtual);
         }
 
         private void tbFormat(TextBox tb)
@@ -58,6 +62,41 @@ namespace Vault
             {
                 e.Handled = true;
             }
+        }
+
+        private void gbxCaixa_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbxOperador_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbxOperador.Enabled = false;
+        }
+
+        private void cbxCheckout_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbxCheckout.Enabled=false;
+        }
+
+        private void lblCheckout_DoubleClick(object sender, EventArgs e)
+        {
+            cbxCheckout.Enabled = true;
+        }
+
+        private void lblOperador_DoubleClick(object sender, EventArgs e)
+        {
+            cbxOperador.Enabled = true;
+        }
+
+        private void tbDinheiro_Leave(object sender, EventArgs e)
+        {
+            tbFormat(tbDinheiro);
+        }
+
+        private void tbDinheiro_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tbValidate(e);
         }
     }
 }
