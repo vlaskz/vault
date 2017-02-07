47db52e991721ac64ebbcf6a89b6e279dac6e4f0
Sam Notebook <dragoonbr@gmail.com>
2017-02-07 21:12:19 -0200

    melhoramento do método Tools.dataParaMySql(com string), remoção de overload para (DateTime). o programa aos poucos se torna mais coeso. Ainda penso em destruir todas as classes e colocar os métodos todos dentro do program.cs. (má ideia)

--- Vault/Tools.cs
+++ Vault/Tools.cs
@@ -5,33 +5,14 @@ namespace Vault
 {
     static class Tools
     {
-       public static string dataParaMySql(DateTime date)
-        {
-            string[] normal = date.ToString().Substring(0, 10).Split('/');
-            Array.Reverse(normal);
-
-            string revert = "";
-
-            foreach (string s in normal)
-                revert += s + '/';
-
-
-            return revert.Substring(0, 10);
-
-        }
-
         public static string dataParaMySql(string date)
         {
             string[] normal = date.Substring(0, 10).Split('/');
             Array.Reverse(normal);
-
             string revert = "";
-
             foreach (string s in normal)
-                revert += s + '/';
-
-
-            return revert.Substring(0, 10);
+                revert += s;
+            return revert;
         }
     }
 }
